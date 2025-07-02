2b7c41af00be4b7e1b914a1d78068f06ddf82fd0
bae080311 <s24024@gsm.hs.kr>
2025-07-03 07:35:53 +0900

    refactor: remove import

--- App.tsx
+++ App.tsx
@@ -3,14 +3,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import './global.css';
 import { SigninPage } from '~/app/signin/page';
 import { SignupPage } from '~/app/signup/page';
-import { Keyboard, Pressable, View } from 'react-native';
+import { Keyboard, Pressable } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Pressable style={{ flex: 1 }} onPress={Keyboard.dismiss}>
+      <Pressable className="flex-1 bg-gray-100 p-6" onPress={Keyboard.dismiss}>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Signup" component={SignupPage} />
           <Stack.Screen name="Signin" component={SigninPage} />
