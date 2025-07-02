7da2a42dc07c7e71cec0482ebbbe4ab7153e3474
bae080311 <s24024@gsm.hs.kr>
2025-07-02 17:24:08 +0900

    Feat: routing

--- App.tsx
+++ App.tsx
@@ -1,5 +1,18 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 import './global.css';
+import { SigninPage } from '~/app/signin/page';
+import { SignupPage } from '~/app/signup/page';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <></>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Signin" component={SigninPage} />
+        <Stack.Screen name="Signup" component={SignupPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
---
+++ src/app/signin/page.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+export function SigninPage() {
+  return <View></View>;
+}
