6de383cc3927362f501e9808cbd54ec1a538e2cb
재욱 <haensol976520@gmail.com>
2025-07-02 17:45:09 +0900

    chore: asile

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,24 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
+  let plugins = [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '~': './src',
+          '@/app': './src/app',
+          '@/shared': './src/shared',
+          '@/entity': './src/entity',
+          '@/view': './src/view',
+          '@/widget': './src/widget',
+        },
+      },
+    ],
+  ];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
     plugins,
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,15 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+config.resolver.alias = {
+  '~': path.resolve(__dirname, 'src'),
+  '@/app': path.resolve(__dirname, 'src/app'),
+  '@/shared': path.resolve(__dirname, 'src/shared'),
+  '@/entity': path.resolve(__dirname, 'src/entity'),
+  '@/view': path.resolve(__dirname, 'src/view'),
+  '@/widget': path.resolve(__dirname, 'src/widget'),
+};
 
 module.exports = withNativeWind(config, { input: './global.css' });
