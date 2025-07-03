bc3ece7bcdc49cffcadcfa9c459a9b520839b064
bae080311 <s24024@gsm.hs.kr>
2025-07-03 23:07:46 +0900

    fix: border color

--- src/entity/main/ui/header/index.tsx
+++ src/entity/main/ui/header/index.tsx
@@ -3,7 +3,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function Header() {
   return (
-    <View className="flex flex-row justify-between border-b px-6 py-4">
+    <View className="flex flex-row justify-between border-b border-gray-400 px-6 py-4">
       <Image
         source={require('~/shared/assets/png/logo.png')}
         className="h-[19px] w-[101px]"
