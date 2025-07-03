9657e6dcd6f4cd3fa34571cf61538bb28ec6b50a
bae080311 <s24024@gsm.hs.kr>
2025-07-03 23:49:37 +0900

    feat: inform

---
+++ src/widget/main/ui/Inform/index.tsx
@@ -0,0 +1,49 @@
+import { Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default function Inform() {
+  return (
+    <View className="flex gap-2 bg-white p-7">
+      <Text className="text-titleSmall">광산구도시재생센터</Text>
+      <Text className="mb-16 text-body2">수완세영</Text>
+      <View className="flex w-full flex-row justify-around">
+        <View
+          style={{
+            backgroundColor: '#fff',
+            borderRadius: 12,
+            padding: 24,
+            shadowColor: '#000',
+            shadowOffset: { width: 2, height: 2 },
+            shadowOpacity: 0.25,
+            shadowRadius: 20,
+            elevation: 5,
+            minHeight: 160,
+            minWidth: 160,
+            alignItems: 'center',
+          }}
+          className="flex justify-between gap-5">
+          <Ionicons name="bag-outline" size={44} color="black" />
+          <Text className="text-titleMedium">물건</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: '#fff',
+            borderRadius: 12,
+            padding: 24,
+            shadowColor: '#000',
+            shadowOffset: { width: 2, height: 2 },
+            shadowOpacity: 0.25,
+            shadowRadius: 20,
+            elevation: 5,
+            minHeight: 160,
+            minWidth: 160,
+          }}
+          className="flex items-center justify-between gap-5">
+          <MaterialCommunityIcons name="headset" size={44} color="black" />
+          <Text className="text-titleMedium">서비스</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
