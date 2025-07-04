9bb9cee98c94c4b5f3b53072999f95055e0c3464
bae080311 <s24024@gsm.hs.kr>
2025-07-04 20:13:14 +0900

    chore: fix tailwind type

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/ ** @type {import('tailwindcss').Config} */;
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./App.{js,ts,tsx}', './src/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
 
