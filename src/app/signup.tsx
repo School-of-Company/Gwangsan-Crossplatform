e25239d1b68bc8806349bf51bfcb904283e2f046
재욱 <haensol976520@gmail.com>
2025-07-04 09:32:48 +0900

    feat: 페이지

--- src/app/signup.tsx
+++ src/app/signup.tsx
@@ -1,12 +1,5 @@
-import { View } from 'react-native';
-import { Dropdown } from '~/shared/ui/Dropdown';
-import { Input } from '~/shared/ui/Input';
+import SignupPageView from '@/view/signup/ui';
 
 export default function SignupPage() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Input label="회원가입" />
-      <Dropdown<string> label="회원가입" placeholder="지점섲낻" items={['exam1', 'exam2']} />
-    </View>
-  );
+  return <SignupPageView />;
 }
