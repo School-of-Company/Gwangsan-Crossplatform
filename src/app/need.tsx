839fb08b11b3ad549d60020f41499ca7ad870136
Ethen1264 <jxorhks86909763@gmail.com>
2025-07-04 10:52:16 +0900

    style: textfield제작

--- src/app/need.tsx
+++ src/app/need.tsx
@@ -1,6 +1,7 @@
 import { ImageUploader } from '~/entity/product/itemForm';
+import { TextField } from '~/shared/ui/TextField';
 import { ItemFormPage } from '~/view/product/itemForm';
 
 export default function Onboarding() {
-  return <ImageUploader />;
+  return <TextField label="내용" />;
 }
---
+++ src/shared/ui/TextField/index.tsx
@@ -0,0 +1,20 @@
+import { TextInput, TextInputProps, View, Text } from 'react-native';
+
+interface TextFieldProps extends TextInputProps {
+  label: string;
+}
+
+export function TextField({ label, ...props }: TextFieldProps) {
+  return (
+    <View className="flex w-full gap-2">
+      <Text className="text-label">{label}</Text>
+      <TextInput
+        className="w-full rounded-xl border border-gray-400 px-4 py-5 text-body5 focus:border-sub2-500"
+        multiline
+        textAlignVertical="top"
+        style={{ minHeight: 160 }}
+        {...props}
+      />
+    </View>
+  );
+}
