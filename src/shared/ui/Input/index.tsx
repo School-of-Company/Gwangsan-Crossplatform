e9035994c452caaf10de21d8c59c709865527ec7
재욱 <haensol976520@gmail.com>
2025-07-04 15:59:47 +0900

    feat: svg 받게 수정

--- src/shared/ui/Input/index.tsx
+++ src/shared/ui/Input/index.tsx
@@ -2,16 +2,24 @@ import { TextInput, TextInputProps, View, Text } from 'react-native';
 
 interface InputProps extends TextInputProps {
   label: string;
+  icon?: React.ReactNode;
 }
 
-export function Input({ label, ...props }: InputProps) {
+export function Input({ label, icon, ...props }: InputProps) {
   return (
     <View className="flex w-full gap-2">
       <Text className="text-label">{label}</Text>
-      <TextInput
-        className="w-full rounded-xl border border-gray-400 px-4 py-5 text-body5 focus:border-sub2-500"
-        {...props}
-      />
+      <View className="relative">
+        <TextInput
+          className="w-full rounded-xl border border-gray-400 px-4 py-5 text-body5 focus:border-sub2-500"
+          {...props}
+        />
+        {icon && (
+          <View className="absolute right-4 top-0 bottom-0 flex justify-center">
+            {icon}
+          </View>
+        )}
+      </View>
     </View>
   );
 }
