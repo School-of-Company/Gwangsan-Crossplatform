54310eecbf05a291d5d3f37b04d00bf7650989a9
bae080311 <s24024@gsm.hs.kr>
2025-07-06 13:13:05 +0900

    remove: 공백

--- src/entity/profile/ui/Light/index.tsx
+++ src/entity/profile/ui/Light/index.tsx
@@ -6,7 +6,7 @@ export default function Light() {
       <Text className="mb-6 text-titleSmall">밝기</Text>
       <View className="relative flex h-5 w-full justify-center">
         <View className="absolute h-5 w-full rounded-xl bg-gray-200 "></View>
-        <View className="max- absolute mx-1 h-3 w-3/5 rounded-xl  bg-sub2-300"></View>
+        <View className="max- absolute mx-1 h-3 w-3/5 rounded-xl bg-sub2-300"></View>
       </View>
       <Text className="ml-auto mt-1 text-sub2-300">8단계</Text>
     </View>
