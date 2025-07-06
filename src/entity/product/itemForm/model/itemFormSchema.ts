f4c1fe55216a14a93a9c5a7e7c6986af6c4ef46d
재욱 <haensol976520@gmail.com>
2025-07-06 17:08:19 +0900

    fix: schema message
    Co-authored-by: Ethen1264 <jxorhks86909763@gmail.com>

--- src/entity/product/itemForm/model/itemFormSchema.ts
+++ src/entity/product/itemForm/model/itemFormSchema.ts
@@ -5,7 +5,7 @@ export const itemFormSchema = z.object({
   mode: z.string(),
   title: z.string().min(1, '제목을 입력해주세요'),
   content: z.string().min(1, '내용을 입력해주세요'),
-  gwangsan: z.number().min(1, '광산을 선택해주세요'),
+  gwangsan: z.number().min(1, '광산을 입력해주세요'),
   imageIds: z.array(z.number()).optional(),
 });
 
