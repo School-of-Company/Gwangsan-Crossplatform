ab13a5a4bc56058a980d156821c259b32b28c073
재욱 <haensol976520@gmail.com>
2025-07-06 17:11:13 +0900

    refector: nativewind로 스타일 적용하게끔 수정
    Co-authored-by: Ethen1264 <jxorhks86909763@gmail.com>

--- src/shared/ui/TextField/index.tsx
+++ src/shared/ui/TextField/index.tsx
@@ -9,10 +9,9 @@ export function TextField({ label, ...props }: TextFieldProps) {
     <View className="flex w-full gap-2">
       <Text className="text-label">{label}</Text>
       <TextInput
-        className="w-full rounded-xl border border-gray-400 px-4 py-5 text-body5 focus:border-sub2-500"
+        className="max-h-[200px] min-h-[120px] w-full rounded-xl border border-gray-400 px-4 py-5 text-body5 focus:border-sub2-500"
         multiline
         textAlignVertical="top"
-        style={{ minHeight: 120, maxHeight: 200 }}
         {...props}
       />
     </View>
