1f0b2e5bb6d6ad891b7aefe53bcaf0ef74b79926
bae080311 <s24024@gsm.hs.kr>
2025-07-06 22:16:15 +0900

    fix: 공백제거

--- src/entity/profile/ui/Light/index.tsx
+++ src/entity/profile/ui/Light/index.tsx
@@ -5,8 +5,8 @@ export default function Light() {
     <View className="mb- px-6 py-10">
       <Text className="mb-6 text-titleSmall">밝기</Text>
       <View className="relative flex h-5 w-full justify-center">
-        <View className="absolute h-5 w-full rounded-xl bg-gray-200 "></View>
-        <View className="max- absolute mx-1 h-3 w-3/5 rounded-xl bg-sub2-300"></View>
+        <View className="absolute h-5 w-full rounded-xl bg-gray-200" />
+        <View className="absolute mx-1 h-3 w-3/5 rounded-xl bg-sub2-300" />
       </View>
       <Text className="ml-auto mt-1 text-sub2-300">8단계</Text>
     </View>
