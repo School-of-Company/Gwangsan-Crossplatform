1586b9b231d053dfd1043877eef87c2693c80df8
bae080311 <s24024@gsm.hs.kr>
2025-07-07 13:47:38 +0900

    feat: mini profile 컴포넌ㄴ트

---
+++ src/entity/post/ui/miniProfile/index.tsx
@@ -0,0 +1,16 @@
+import { Image, Text, View } from 'react-native';
+
+export default function MiniProfile() {
+  return (
+    <View className=" flex-row items-center justify-between border-b border-b-gray-100 px-6 py-3">
+      <View className="flex-row gap-3">
+        <Image className="size-[50px]" source={require('~/shared/assets/png/defaultProfile.png')} />
+        <View className="gap-[5px]">
+          <Text className="text-body3">모태환</Text>
+          <Text>첨단 1동</Text>
+        </View>
+      </View>
+      <Text className="text-body1 text-sub2-500">8단계</Text>
+    </View>
+  );
+}
