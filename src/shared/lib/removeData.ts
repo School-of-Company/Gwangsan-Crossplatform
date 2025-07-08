36c6ad1d482c12e88c9e77a754880b8ee77219d1
bae080311 <s24024@gsm.hs.kr>
2025-07-08 22:54:01 +0900

    feat: handle async storage

---
+++ src/shared/lib/removeData.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-toast-message';
+
+export const removeData = async (name: string) => {
+  try {
+    AsyncStorage.removeItem(name);
+  } catch (e) {
+    Toast.show({
+      type: 'error',
+      text1: '오류 발생',
+      text2: `데이터를 삭제하는 중 오류가 발생했습니다`,
+    });
+  }
+};
---
+++ src/shared/lib/setData.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-toast-message';
+
+export const setData = async (name: string, data: string) => {
+  try {
+    AsyncStorage.setItem(name, data);
+  } catch (e) {
+    Toast.show({
+      type: 'error',
+      text1: '오류 발생',
+      text2: `데이터를 저장하는 중 오류가 발생했습니다`,
+    });
+  }
+};
