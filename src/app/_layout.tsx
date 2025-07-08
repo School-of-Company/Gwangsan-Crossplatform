ec42740c6c19f92a17c30c62b35cf50269c2bd9f
bae080311 <s24024@gsm.hs.kr>
2025-07-08 22:53:40 +0900

    feat: providers

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,20 +2,25 @@ import { Stack } from 'expo-router';
 import { View } from 'react-native';
 import '../../global.css';
 import { useCustomFonts } from '@/shared/assets/fonts/fontLoader';
+import { QueryProvider } from './QueryProvider';
+import Toast from 'react-native-toast-message';
 
 export default function RootLayout() {
   const fontsLoaded = useCustomFonts();
   if (!fontsLoaded) return null;
   return (
     <View className="mb-6 flex-1 bg-white">
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          animation: 'slide_from_right',
-          gestureEnabled: true,
-          gestureDirection: 'horizontal',
-        }}
-      />
+      <QueryProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            animation: 'slide_from_right',
+            gestureEnabled: true,
+            gestureDirection: 'horizontal',
+          }}
+        />
+        <Toast />
+      </QueryProvider>
     </View>
   );
 }
