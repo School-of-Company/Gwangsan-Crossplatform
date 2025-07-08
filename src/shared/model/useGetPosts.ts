929bbc9db61ae61162cc60e5ecf32e3c7aaf8702
bae080311 <s24024@gsm.hs.kr>
2025-07-08 23:44:58 +0900

    feat: get post api request

---
+++ src/shared/model/useGetPosts.ts
@@ -0,0 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
+import { getPosts } from '../api/getPosts';
+import { MODE, PostType, TYPE } from '../types/postType';
+
+export const useGetPosts = (mode: MODE, type: TYPE) => {
+  return useQuery<PostType>({
+    queryKey: ['posts', mode, type],
+    queryFn: () => getPosts(type, mode),
+  });
+};
