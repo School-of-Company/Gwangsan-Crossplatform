549a62380b0bf9f906a0139f47f934ca17374021
bae080311 <s24024@gsm.hs.kr>
2025-07-10 00:04:06 +0900

    fix: post nullable

--- src/shared/api/getPosts.ts
+++ src/shared/api/getPosts.ts
@@ -2,9 +2,12 @@ import Toast from 'react-native-toast-message';
 import { instance } from '../lib/aixos';
 import { MODE, TYPE } from '../types/postType';
 
-export const getPosts = async (type: TYPE, mode: MODE) => {
+export const getPosts = async (type?: TYPE, mode?: MODE) => {
   try {
-    return (await instance.get('/post?type=' + type + '&mode=' + mode)).data;
+    const params = new URLSearchParams();
+    if (type) params.append('type', type);
+    if (mode) params.append('mode', mode);
+    return (await instance.get('/post?' + params.toString())).data;
   } catch (error) {
     if (error instanceof Error) {
       Toast.show({
