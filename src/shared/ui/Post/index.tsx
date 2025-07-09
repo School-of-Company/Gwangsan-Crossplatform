71da3ac5daa393665a1a85ba1614a2f0cbdc5e6f
bae080311 <s24024@gsm.hs.kr>
2025-07-09 15:13:43 +0900

    feat: onPress

--- src/shared/ui/Post/index.tsx
+++ src/shared/ui/Post/index.tsx
@@ -1,9 +1,15 @@
-import { Image, Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { useCallback } from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { PostType } from '~/shared/types/postType';
 
 export default function Post(data: PostType) {
+  const router = useRouter();
+  const handlePress = useCallback(() => {
+    router.push('/post/' + data.id);
+  }, [router]);
   return (
-    <View className="flex flex-row items-center gap-6 px-6 py-4">
+    <TouchableOpacity onPress={handlePress} className="flex flex-row items-center gap-6 px-6 py-4">
       {data.imageUrls.length > 0 ? (
         data.imageUrls.map((image) => {
           return (
@@ -24,6 +30,6 @@ export default function Post(data: PostType) {
         <Text className="text-body3">{data.title}</Text>
         <Text className="text-body5 text-gray-300">{data.gwangsan}광산</Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
