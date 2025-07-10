a84ddbe1ec3054d6ee7f4372eaaa0cb9c7dd7b9b
bae080311 <s24024@gsm.hs.kr>
2025-07-10 09:14:21 +0900

    fix: button location

--- src/view/transaction/ui/TransactionPage/index.tsx
+++ src/view/transaction/ui/TransactionPage/index.tsx
@@ -1,19 +1,24 @@
-import { useLocalSearchParams } from 'expo-router';
-import { useState } from 'react';
-import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { useGetPosts } from '~/shared/model/useGetPosts';
 import { TYPE } from '~/shared/types/postType';
 import { Header } from '~/shared/ui';
 import { handleCategory, returnValue } from '../../model/handleCategory';
 import { Category } from '../../model/category';
 import Post from '~/shared/ui/Post';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function TransactionPageView() {
   const { type } = useLocalSearchParams<{ type: TYPE }>();
   const [category, setCategory] = useState<Category>();
   const mode = category ? returnValue(category) : undefined;
+  const R = useRouter();
   const { data } = useGetPosts(mode!, type);
 
+  const handlePress = useCallback(() => {
+    R.push('/need');
+  }, [R]);
   return (
     <SafeAreaView className="android:pt-10 h-full bg-white">
       <Header headerTitle={type === 'SERVICE' ? '서비스' : '물건'} />
@@ -36,6 +41,14 @@ export default function TransactionPageView() {
           return <Post key={v.id} {...v} />;
         })}
       </ScrollView>
+      <TouchableOpacity onPress={handlePress}>
+        <Ionicons
+          name="add-circle"
+          size={60}
+          color="#8FC31D"
+          className="absolute bottom-10 right-10"
+        />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
