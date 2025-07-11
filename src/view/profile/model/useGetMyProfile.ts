9120f28ed7ee97c3756ae74aa1b81d463802043b
bae080311 <s24024@gsm.hs.kr>
2025-07-11 15:18:57 +0900

    feat: type

--- src/view/profile/model/useGetMyProfile.ts
+++ src/view/profile/model/useGetMyProfile.ts
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { getMyProfile } from '../api/getMyProfile';
+import { ProfileType } from './profileType';
 
 export const useGetMyProfile = () => {
-  return useQuery({
+  return useQuery<ProfileType>({
     queryKey: ['myProfile'],
     queryFn: getMyProfile,
   });
