2a59434ccec33ab53c2c09cf87f80ce64d5d1df5
bae080311 <s24024@gsm.hs.kr>
2025-07-11 16:02:19 +0900

    fix: light 올림

--- src/entity/profile/ui/Light/index.tsx
+++ src/entity/profile/ui/Light/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from 'react-native';
+import { getLightColor } from '~/shared/lib/handleLightColor';
 
 interface LightProps {
   lightLevel?: number;
@@ -9,9 +10,11 @@ export default function Light({ lightLevel = 1 }: LightProps) {
     <View className="mb- px-6 py-10">
       <Text className="mb-6 text-titleSmall">밝기</Text>
       <View className="relative flex h-5 w-full justify-center rounded-xl bg-gray-200">
-        <View className={`absolute mx-1 h-3 w-[${lightLevel}%] rounded-xl bg-sub2-300`} />
+        <View
+          className={`absolute mx-1 h-3 w-[${lightLevel}%] rounded-xl bg-${getLightColor(lightLevel)}`}
+        />
       </View>
-      <Text className="ml-auto mt-1 text-sub2-300">{lightLevel / 10}</Text>
+      <Text className="ml-auto mt-1 text-sub2-300">{Math.ceil(lightLevel / 10)}단계</Text>
     </View>
   );
 }
