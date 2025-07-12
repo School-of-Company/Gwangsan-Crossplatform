72a1552afd1781cd817b211ce7fbe2974a32929c
bae080311 <s24024@gsm.hs.kr>
2025-07-12 14:08:49 +0900

    remove: type

--- src/entity/reviews/model/reviewPostType.ts
+++
@@ -1,9 +0,0 @@
-import { ImageType } from '~/shared/types/imageType';
-
-export interface ReviewPostType {
-  producerId: number;
-  content: string;
-  light: number;
-  reviewerName: string;
-  imageUrls: ImageType[];
-}
