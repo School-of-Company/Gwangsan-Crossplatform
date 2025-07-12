e5842c9cd0eb57697069c6e3d72b576c24d3c770
bae080311 <s24024@gsm.hs.kr>
2025-07-12 14:14:14 +0900

    feat: map review post

--- src/entity/reviews/ui/ReviewPost/index.tsx
+++ src/entity/reviews/ui/ReviewPost/index.tsx
@@ -1,21 +1,28 @@
 import { Image, Text, View } from 'react-native';
+import { getLightColor } from '~/shared/lib/handleLightColor';
+import { ReviewPostType } from '~/view/reviews/model/reviewPostType';
 
-export default function ReviewPost() {
+interface ReviewPostProps {
+  review: ReviewPostType;
+}
+
+export default function ReviewPost({ review }: ReviewPostProps) {
   return (
     <View className="flex flex-row gap-9 border-b border-b-gray-200 px-6 py-3">
-      <Image
-        source={require('@/shared/assets/png/onboardingSlide1.png')}
-        className="size-24 rounded-lg"
-      />
+      {review.images.map((image, index) => (
+        <Image key={index} source={{ uri: image.imageUrl }} className="size-24 rounded-lg" />
+      ))}
       <View>
-        <View className="relative mb-3 flex w-1/2 justify-center">
-          <View className="absolute h-3 w-full rounded-xl bg-gray-200 "></View>
-          <View className="max- absolute mx-0.5 h-2 w-3/5 rounded-xl  bg-sub2-300"></View>
+        <View className="relative flex h-5 w-full justify-center rounded-xl bg-gray-200">
+          <View
+            style={{ width: `${review.light}%` }}
+            className={`absolute mx-1 h-3 rounded-xl bg-${getLightColor(review.light)}`}
+          />
         </View>
         <Text className="mb-1 max-w-[200px] flex-wrap text-label text-[#555555]">
-          핸드폰 컬러가 마음에 들고 상태도 매우 좋아요
+          {review.content}
         </Text>
-        <Text className="text-label">모태환</Text>
+        <Text className="text-label">{review.reviewerName}</Text>
       </View>
     </View>
   );
