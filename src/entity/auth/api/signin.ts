15733a086c753d52011227a9a11b322cfb80cb86
재욱 <haensol976520@gmail.com>
2025-07-13 02:13:17 +0900

    fix: auth 로직 인터셉터 안쓰게 수정

--- src/entity/auth/api/signin.ts
+++ src/entity/auth/api/signin.ts
@@ -2,10 +2,19 @@ import { instance } from '@/shared/lib/axios';
 import { setData } from '@/shared/lib/setData';
 import { getDeviceInfo } from '@/shared/model/getDeviceInfo';
 import { SigninFormData, AuthResponse } from '~/entity/auth/model/authState';
+import axios from 'axios';
+
+const auth = axios.create({
+  baseURL: instance.defaults.baseURL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
 const signin = async (formData: SigninFormData): Promise<AuthResponse> => {
   try {
-    const response = await instance.post<AuthResponse>('/auth/signin', {
+    const response = await auth.post<AuthResponse>('/auth/signin', {
       nickname: formData.nickname,
       password: formData.password,
       deviceToken: formData.deviceToken,
@@ -37,7 +46,7 @@ export const signinWithDeviceInfo = async (credentials: {
 
     return await signin(formData);
   } catch (error) {
-    console.error('로그인 실패:', error);
+    console.error(error);
     throw error;
   }
 };
