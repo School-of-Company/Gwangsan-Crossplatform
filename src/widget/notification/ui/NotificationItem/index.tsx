297f146964d87cc86c56e0c23f73b6c60f7d2b58
재욱 <haensol976520@gmail.com>
2025-07-18 00:47:28 +0900

    feat: 페이지에 연결

--- src/widget/notification/ui/NotificationItem/index.tsx
+++ src/widget/notification/ui/NotificationItem/index.tsx
@@ -1,49 +1,44 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native';
-import { useRouter } from 'expo-router';
-import { ImageType } from '~/shared/types/imageType';
+// import { useRouter } from 'expo-router';
+import { AlertType } from '~/entity/notification/model/alertTypes';
+import { formatDate } from '~/shared/lib/formatDate';
 
 interface NotificationItemProps {
   id: number;
   title: string;
   content: string;
-  images: ImageType[];
-  place?: string;
-  createdAt?: string;
-  role?: string;
+  alertType: AlertType;
+  imageIds: number[];
+  createdAt: string;
 }
 
 const NotificationItem = ({
-  id,
+  // id,
   title,
   content,
-  createdAt = '',
-  images,
+  // alertType,
+  // imageIds,
+  createdAt,
 }: NotificationItemProps) => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  const handlePress = () => {
-    router.push(`/notification/${id}`);
-  };
+  // const handlePress = () => {
+  //   router.push(`/notification/${id}`);
+  // };
 
-  const hasImages = images && images.length > 0;
+  const displayImage = require('~/shared/assets/png/gwangsanLogo.png');
 
   return (
-    <TouchableOpacity onPress={handlePress} className="mb-3 bg-white p-4" activeOpacity={0.7}>
+    <TouchableOpacity className="mb-3 bg-white p-4" activeOpacity={0.7}>
       <View className="flex-row">
-        {hasImages && (
-          <View className="mr-3">
-            <Image
-              source={{ uri: images[0].imageUrl }}
-              className="h-16 w-16 rounded-lg"
-              resizeMode="cover"
-            />
-          </View>
-        )}
+        <View className="mr-3">
+          <Image source={displayImage} className="h-16 w-16 rounded-lg" resizeMode="cover" />
+        </View>
 
         <View className="flex-1">
           <View className="mb-2 flex-row items-start justify-between">
             <Text className="mr-2 flex-1 text-lg font-semibold text-gray-900">{title}</Text>
-            <Text className="text-sm text-gray-500">{createdAt}</Text>
+            <Text className="text-sm text-gray-500">{formatDate(createdAt)}</Text>
           </View>
 
           <Text className="text-sm leading-5 text-gray-500" numberOfLines={1} ellipsizeMode="tail">
