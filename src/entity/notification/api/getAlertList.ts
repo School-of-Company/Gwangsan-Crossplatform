c086a88b695ce93ffc0a1fb9cfcd31a41cf7e9ed
재욱 <haensol976520@gmail.com>
2025-07-19 09:29:14 +0900

    feat: 예외 throw 추가

--- src/entity/notification/api/getAlertList.ts
+++ src/entity/notification/api/getAlertList.ts
@@ -2,6 +2,10 @@ import { instance } from '~/shared/lib/axios';
 import { AlertListResponse } from '../model/alertTypes';
 
 export const getAlertList = async (): Promise<AlertListResponse> => {
-  const { data } = await instance.get<AlertListResponse>('/alert');
-  return data;
+  try {
+    const { data } = await instance.get<AlertListResponse>('/alert');
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
