0230e88a842de38bf417195ae243aa92f98667fa
재욱 <haensol976520@gmail.com>
2025-07-24 16:16:07 +0900

    fix: 로그아웃 시 스토리지 정리

--- src/entity/auth/model/useSignout.ts
+++ src/entity/auth/model/useSignout.ts
@@ -3,6 +3,7 @@ import { useCallback } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { signout } from '../api/signout';
 import { removeData } from '~/shared/lib/removeData';
+import { clearCurrentUserId } from '~/shared/lib/getCurrentUserId';
 
 export const useSignout = () => {
   const router = useRouter();
@@ -24,6 +25,10 @@ export const useSignout = () => {
   const handleSignout = useCallback(() => {
     removeData('accessToken');
     removeData('refreshToken');
+    removeData('memberId');
+    
+    clearCurrentUserId();
+    
     signoutMutation.mutate();
   }, [signoutMutation]);
 
