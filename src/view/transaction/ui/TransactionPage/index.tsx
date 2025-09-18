c57e717ecfc914686c37670a299dc377456d7a55
bae080311 <s24024@gsm.hs.kr>
2025-09-18 12:21:57 +0900

    remove: 글작성 버튼

--- src/view/transaction/ui/TransactionPage/index.tsx
+++ src/view/transaction/ui/TransactionPage/index.tsx
@@ -1,4 +1,4 @@
-import { useLocalSearchParams, useRouter } from 'expo-router';
+import { useLocalSearchParams } from 'expo-router';
 import { useCallback, useState, useEffect, useRef } from 'react';
 import { RefreshControl, ScrollView, Text, TouchableOpacity, View, Animated } from 'react-native';
 import { useGetPosts } from '~/shared/model/useGetPosts';
@@ -7,19 +7,7 @@ import { Header } from '~/shared/ui';
 import { handleCategory, returnValue } from '../../model/handleCategory';
 import { Category } from '../../model/category';
 import Post from '~/shared/ui/Post';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-const ROUTE_MAP: Record<TYPE, Record<MODE, string>> = {
-  SERVICE: {
-    RECEIVER: '/request',
-    GIVER: '/offer',
-  },
-  OBJECT: {
-    RECEIVER: '/need',
-    GIVER: '/sell',
-  },
-};
 
 export default function TransactionPageView() {
   const { type, mode } = useLocalSearchParams<{ type: TYPE; mode?: MODE }>();
@@ -38,7 +26,6 @@ export default function TransactionPageView() {
   const [category, setCategory] = useState<Category>(getInitialCategory());
   const [refreshing, setRefreshing] = useState(false);
   const currentMode = category ? returnValue(category) : undefined;
-  const router = useRouter();
 
   const slideAnimation = useRef(new Animated.Value(0)).current;
   const categories = handleCategory(type as TYPE) ?? [];
@@ -65,14 +52,6 @@ export default function TransactionPageView() {
       setRefreshing(false);
     }
   }, [refetch]);
-
-  const handlePress = useCallback(() => {
-    if (!type || !currentMode) return;
-    const targetRoute = ROUTE_MAP[type as TYPE]?.[currentMode as MODE];
-    if (targetRoute) {
-      router.push(targetRoute);
-    }
-  }, [router, type, currentMode]);
 
   return (
     <SafeAreaView className="flex-1 bg-white">
@@ -110,15 +89,6 @@ export default function TransactionPageView() {
           <Post key={v.id} {...v} />
         ))}
       </ScrollView>
-      {type && currentMode && (
-        <TouchableOpacity
-          className="absolute bottom-10 right-10 z-50 h-[60px] w-[60px]"
-          hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
-          activeOpacity={0.7}
-          onPress={handlePress}>
-          <Ionicons name="add-circle" size={60} color="#8FC31D" />
-        </TouchableOpacity>
-      )}
     </SafeAreaView>
   );
 }
