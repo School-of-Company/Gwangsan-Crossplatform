0367063bab94f043197ba58c112e2869d3a7fd8b
bae080311 <s24024@gsm.hs.kr>
2025-10-08 21:52:11 +0900

    fix: type

--- src/view/transaction/model/handleCategory.ts
+++ src/view/transaction/model/handleCategory.ts
@@ -1,7 +1,8 @@
-import { MODE, TYPE } from '~/shared/types/postType';
+import { ModeType } from '~/shared/types/mode';
 import { Category } from './category';
+import { ProductType } from '~/shared/types/type';
 
-export const handleCategory = (category: TYPE) => {
+export const handleCategory = (category: ProductType) => {
   switch (category) {
     case 'OBJECT':
       return ['팔아요', '필요해요'];
@@ -10,7 +11,7 @@ export const handleCategory = (category: TYPE) => {
   }
 };
 
-export const returnValue = (value: Category | undefined): MODE | undefined => {
+export const returnValue = (value: Category | undefined): ModeType | undefined => {
   switch (value) {
     case '팔아요':
     case '할 수 있어요':
