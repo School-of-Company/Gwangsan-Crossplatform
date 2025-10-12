368769bccb523e96b8c10791051ff88f8b749763
재욱 <haensol976520@gmail.com>
2025-10-12 19:51:16 +0900

    feat: api 코드

---
+++ src/entity/auth/api/resetPassword.ts
@@ -0,0 +1,23 @@
+
+export interface ResetPasswordRequest {
+  phoneNumber: string;
+  newPassword: string;
+}
+
+export const resetPassword = async (
+  request: ResetPasswordRequest
+): Promise<Response> => {
+  const response = await fetch('/auth/password', {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(request),
+  });
+
+  if (!response.ok) {
+    throw new Error(response.status.toString());
+  }
+
+  return response.json();
+};
---
+++ src/entity/auth/api/verifyPasswordResetSms.ts
@@ -0,0 +1,23 @@
+
+export interface VerifyPasswordResetSmsRequest {
+  phoneNumber: string;
+  code: string;
+}
+
+export const verifyPasswordResetSms = async (
+  request: VerifyPasswordResetSmsRequest
+): Promise<Response> => {
+  const response = await fetch('/sms/password/verify', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(request),
+  });
+
+  if (!response.ok) {
+    throw new Error(response.status.toString());
+  }
+
+  return response.json();
+};
