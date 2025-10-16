a884929d2015ba8e9fd0326ae83cd45bc22bfa86
bae080311 <s24024@gsm.hs.kr>
2025-10-16 18:09:46 +0900

    feat: 본점 타입

--- src/shared/consts/place.ts
+++ src/shared/consts/place.ts
@@ -14,3 +14,13 @@ export const PLACES = [
 ] as const;
 
 export type Place = (typeof PLACES)[number];
+
+export const HEAD = {
+  1: '광산구도시재생공동체센터',
+  2: '광산구자원봉사센터',
+  3: '광산구지역사회보장협의체',
+  4: '투게더광산나눔문화센터',
+} as const;
+
+export type HeadKey = keyof typeof HEAD;
+export type HeadValue = (typeof HEAD)[HeadKey];
