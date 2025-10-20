74e99c30f9ad46f8d5d915cc2fbfdb1355a05152
bae080311 <s24024@gsm.hs.kr>
2025-10-20 14:15:04 +0900

    feat: cancelTrade

---
+++ src/view/cancelTrade/api/cancelTrade.ts
@@ -0,0 +1,13 @@
+import { instance } from '~/shared/lib/axios';
+
+export const cancelTrade = async (productId: string, imageIds: string[], reason: string) => {
+  try {
+    const res = await instance.post('/trade/cancel/' + productId, {
+      imageIds,
+      reason,
+    });
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
