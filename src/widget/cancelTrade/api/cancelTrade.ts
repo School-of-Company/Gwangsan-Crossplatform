15edc8f427432fc28f99d3e0391e0fae5d685628
bae080311 <s24024@gsm.hs.kr>
2025-10-22 18:18:41 +0900

    feat: use cancel trade

--- src/widget/cancelTrade/api/cancelTrade.ts
+++ src/widget/cancelTrade/api/cancelTrade.ts
@@ -1,6 +1,6 @@
 import { instance } from '~/shared/lib/axios';
 
-export const cancelTrade = async (productId: string, imageIds: string[], reason: string) => {
+export const cancelTrade = async (productId: string, imageIds: number[], reason: string) => {
   try {
     const res = await instance.post('/trade/cancel/' + productId, {
       imageIds,
