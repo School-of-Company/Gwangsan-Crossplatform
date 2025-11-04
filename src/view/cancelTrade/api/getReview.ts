b9a28d737f0ee389344add1f99c0f357e329cfa2
bae080311 <s24024@gsm.hs.kr>
2025-11-04 11:53:04 +0900

    fix: 엔드포인트

--- src/view/cancelTrade/api/getReview.ts
+++ src/view/cancelTrade/api/getReview.ts
@@ -2,7 +2,7 @@ import { instance } from '~/shared/lib/axios';
 
 export const getReview = async (id: string) => {
   try {
-    const res = await instance.get('/review/' + id);
+    const res = await instance.get('/review/detail/' + id);
     return res.data;
   } catch (error) {
     throw error;
