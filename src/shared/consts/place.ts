05a8b6108b5f2de0a0da0f2845a116f8c6bc487e
재욱 <haensol976520@gmail.com>
2025-12-17 08:37:22 +0900

    fix: PLACE_ITEMS constant containing the mapping of id

--- src/shared/consts/place.ts
+++ src/shared/consts/place.ts
@@ -1,25 +1,30 @@
-export const PLACES = [
-  '고실마을',
-  '수완마을',
-  '신가',
-  '신창',
-  '도산',
-  '우산',
-  '월곡1',
-  '첨단2',
-  '평동',
-  '월곡2',
-  '하남',
-  '도시재생공동체센터',
+export const PLACE_ITEMS = [
+  { id: 1, name: '수완마을' },
+  { id: 2, name: '고실마을' },
+  { id: 3, name: '신가' },
+  { id: 4, name: '신창' },
+  { id: 5, name: '도산' },
+  { id: 6, name: '우산' },
+  { id: 7, name: '월곡1' },
+  { id: 8, name: '첨단2' },
+  { id: 9, name: '월곡2' },
+  { id: 10, name: '하남' },
+  { id: 11, name: '평동' },
+  { id: 12, name: '광산구도시재생공동체센터' },
+  { id: 13, name: '광산구자원봉사센터' },
+  { id: 14, name: '광산구지역사회보장협의체' },
+  { id: 15, name: '투게더광산나눔문화센터' },
 ] as const;
+
+export const PLACES = PLACE_ITEMS.map((item) => item.name);
 
 export type Place = (typeof PLACES)[number];
 
 export const HEAD = {
-  1: '광산구도시재생공동체센터',
-  2: '광산구자원봉사센터',
-  3: '광산구지역사회보장협의체',
-  4: '투게더광산나눔문화센터',
+  12: '광산구도시재생공동체센터',
+  13: '광산구자원봉사센터',
+  14: '광산구지역사회보장협의체',
+  15: '투게더광산나눔문화센터',
 } as const;
 
 export type HeadKey = keyof typeof HEAD;
