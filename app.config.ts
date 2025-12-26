bb4afddf603d76dfa7cfa45c45ed56a93a9f024b
bae080311 <s24024@gsm.hs.kr>
2025-12-27 08:50:17 +0900

    chore: plugin

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,7 @@ import { ExpoConfig } from '@expo/config';
 export default ({ config }: { config: ExpoConfig }) => {
   return {
     ...config,
+    plugins: ['@sentry/react-native'],
     extra: {
       ...config.extra,
       apiUrl: process.env.API_URL,
