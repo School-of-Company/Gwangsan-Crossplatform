da51e7479444d533c0337cab3c0ea47e2c1aa864
bae080311 <s24024@gsm.hs.kr>
2026-03-09 14:14:37 +0900

    fix: lint error

--- src/shared/ui/PasswordInput/index.tsx
+++ src/shared/ui/PasswordInput/index.tsx
@@ -7,20 +7,22 @@ interface PasswordInputProps extends Omit<TextInputProps, 'secureTextEntry'> {
   label: string;
 }
 
-export const PasswordInput = forwardRef<TextInput, PasswordInputProps>(({ label, ...props }, ref) => {
-  const [isVisible, setIsVisible] = useState(false);
+export const PasswordInput = forwardRef<TextInput, PasswordInputProps>(
+  ({ label, ...props }, ref) => {
+    const [isVisible, setIsVisible] = useState(false);
 
-  return (
-    <Input
-      ref={ref}
-      label={label}
-      secureTextEntry={!isVisible}
-      icon={
-        <TouchableOpacity onPress={() => setIsVisible((prev) => !prev)}>
-          <Icon name={isVisible ? 'eye-outline' : 'eye-off-outline'} size={20} color="#9CA3AF" />
-        </TouchableOpacity>
-      }
-      {...props}
-    />
-  );
-});
+    return (
+      <Input
+        ref={ref}
+        label={label}
+        secureTextEntry={!isVisible}
+        icon={
+          <TouchableOpacity onPress={() => setIsVisible((prev) => !prev)}>
+            <Icon name={isVisible ? 'eye-outline' : 'eye-off-outline'} size={20} color="#9CA3AF" />
+          </TouchableOpacity>
+        }
+        {...props}
+      />
+    );
+  }
+);
