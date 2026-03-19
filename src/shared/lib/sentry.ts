276dc4f76ab1e185c9b654b6230c83a604c9c8ff
bae080311 <s24024@gsm.hs.kr>
2026-03-20 08:50:41 +0900

    chore: sentry

--- src/shared/lib/sentry.ts
+++ src/shared/lib/sentry.ts
@@ -2,4 +2,9 @@ import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
   dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+  ],
 });
