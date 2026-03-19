f99fcac7d39071ed222434b54c8785b8b3bc2ef1
bae080311 <s24024@gsm.hs.kr>
2026-03-20 08:53:17 +0900

    fix: code style

--- src/shared/lib/sentry.ts
+++ src/shared/lib/sentry.ts
@@ -4,7 +4,5 @@ Sentry.init({
   dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
-  integrations: [
-    Sentry.mobileReplayIntegration(),
-  ],
+  integrations: [Sentry.mobileReplayIntegration()],
 });
