3711a6f1d8589466770305ff37d08a668d732286
재욱 <haensol976520@gmail.com>
2026-03-20 09:59:56 +0900

    feat: add release configurations for Detox E2E tests on iOS and Android

--- .detoxrc.js
+++ .detoxrc.js
@@ -34,6 +34,26 @@ module.exports = {
         'build',
       ].join(' '),
     },
+    'ios.release': {
+      type: 'ios.app',
+      binaryPath: path.join(root, 'ios/build/Build/Products/Release-iphonesimulator/app.app'),
+      build: [
+        'xcodebuild',
+        `-workspace ${path.join(root, 'ios/app.xcworkspace')}`,
+        '-scheme app',
+        '-configuration Release',
+        '-sdk iphonesimulator',
+        `-derivedDataPath ${path.join(root, 'ios/build')}`,
+        'ARCHS=arm64',
+        '"EXCLUDED_ARCHS[sdk=iphonesimulator*]=x86_64"',
+        'ONLY_ACTIVE_ARCH=YES',
+        'CODE_SIGN_IDENTITY=""',
+        'CODE_SIGNING_REQUIRED=NO',
+        'CODE_SIGNING_ALLOWED=NO',
+        '-quiet',
+        'build',
+      ].join(' '),
+    },
     'android.debug': {
       type: 'android.apk',
       binaryPath: path.join(root, 'android/app/build/outputs/apk/debug/app-debug.apk'),
@@ -43,6 +63,15 @@ module.exports = {
       ),
       build: `cd ${path.join(root, 'android')} && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug --stacktrace`,
       reversePorts: [8081],
+    },
+    'android.release': {
+      type: 'android.apk',
+      binaryPath: path.join(root, 'android/app/build/outputs/apk/release/app-release.apk'),
+      testBinaryPath: path.join(
+        root,
+        'android/app/build/outputs/apk/androidTest/release/app-release-androidTest.apk'
+      ),
+      build: `cd ${path.join(root, 'android')} && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release --stacktrace`,
     },
   },
   devices: {
@@ -64,9 +93,17 @@ module.exports = {
       device: 'simulator',
       app: 'ios.debug',
     },
+    'ios.sim.release': {
+      device: 'simulator',
+      app: 'ios.release',
+    },
     'android.emu.debug': {
       device: 'emulator',
       app: 'android.debug',
+    },
+    'android.emu.release': {
+      device: 'emulator',
+      app: 'android.release',
     },
   },
 };
