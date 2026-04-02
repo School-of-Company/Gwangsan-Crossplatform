e895e38487ae9154ac90e7c4b8dcb11ae92de3c9
재욱 <haensol976520@gmail.com>
2026-04-03 08:08:05 +0900

    feat: 채팅 페이지에 에러 바운더리 적용

--- src/app/chatting/index.tsx
+++ src/app/chatting/index.tsx
@@ -1,5 +1,10 @@
+import { FeatureErrorBoundary } from '@/shared/ui/FeatureErrorBoundary';
 import ChatRoomListPage from '@/view/chat/ui/ChatRoomListPage';
 
 export default function Chatting() {
-  return <ChatRoomListPage />;
+  return (
+    <FeatureErrorBoundary>
+      <ChatRoomListPage />
+    </FeatureErrorBoundary>
+  );
 }
